649824930f64e35125652851f848d20a279c4571
RealArtisanFoods <105236007+RealArtisanFoods@users.noreply.github.com>
2023-03-03 12:56:38 -0600

    Complete 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+function isDivisible(n, d)
+{
+    return !(n % d);
+}
 
+const leapYears = function(year) {
+    if (!isDivisible(year, 4)) return false;
+    if (isDivisible(year, 100) && !isDivisible(year, 400)) return false;
+    return true;
 };
 
 // Do not edit below this line
