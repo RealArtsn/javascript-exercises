d9bef57a921bc039569a2952fdadd8aa7419b5d4
RealArtisanFoods <105236007+RealArtisanFoods@users.noreply.github.com>
2023-03-03 12:25:47 -0600

    04_removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray) {
+    for (arg of arguments)
+    {
+        if (!['string','number'].includes(typeof arg)) continue;
+        let idx = inputArray.indexOf(arg);
+        if (idx < 0) continue;
+        inputArray.splice(idx, 1);
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
