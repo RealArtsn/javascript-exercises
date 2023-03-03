f65db07a70622f6fee149167e1d6c014d2e2d1f1
RealArtisanFoods <105236007+RealArtisanFoods@users.noreply.github.com>
2023-03-03 13:47:50 -0600

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,44 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	let total = 0;
+  for (num of numArray)
+  {
+    total += num;
+  }
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  let total = numArray[0]
+  for (let i = 1; i < numArray.length; i++)
+  {
+    total *= numArray[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) return 1;
+  let total = a;
+  for (let i = a - 1; i > 0; i--)
+  {
+    total *= i;
+  }
+  return total;
 };
+
+console.log(multiply(1,3))
 
 // Do not edit below this line
 module.exports = {
