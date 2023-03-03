6f909f39802dff54e0098059dda02c2b523581b8
RealArtisanFoods <105236007+RealArtisanFoods@users.noreply.github.com>
2023-03-03 17:49:12 -0600

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    // remove punctuation
+    PUNCTUATION = ['.',',','!', ' ']
+    let filteredCharArray = [];
+    for (let i = 0; i < inputString.length; i++)
+    {
+        if (PUNCTUATION.includes(inputString[i])) continue;
+        filteredCharArray.unshift(inputString[i].toLowerCase());
+    }
+    // check equality from front and back until meet in middle
+    for (let i = 0; i < (filteredCharArray.length / 2); i++)
+    {
+        if (filteredCharArray[i] !== filteredCharArray[filteredCharArray.length - 1 - i])
+        {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
