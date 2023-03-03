90be146e6ff3aad6813db61fea7fe6eb6c38e202
RealArtisanFoods <105236007+RealArtisanFoods@users.noreply.github.com>
2023-03-03 11:53:06 -0600

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    if (n < 0)
+    {
+        return 'ERROR';
+    }
+    let repeated = '';
+    for (let i = 0; i < n; i++)
+    {
+        repeated += str;
+    }
+    return repeated;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
