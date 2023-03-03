8e5477a98eb84adb21eb343ac2213b5fe1e1cee9
RealArtisanFoods <105236007+RealArtisanFoods@users.noreply.github.com>
2023-03-03 12:43:07 -0600

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let args = [a, b].sort();
+    if ((typeof args[0] !== 'number' || typeof args[1] !== 'number') || (args[0] < 0 || args[1] < 0))
+    {
+        return 'ERROR';
+    }
+    let sum = 0;
+    for (let i = args[0]; i <= args[1]; i++)
+    {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
