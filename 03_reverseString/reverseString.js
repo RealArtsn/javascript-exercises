ac85006aeff04be6ba88b599c8bf1d79dab6c6dd
RealArtisanFoods <105236007+RealArtisanFoods@users.noreply.github.com>
2023-03-03 12:03:34 -0600

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    let stringArray = s.split('');
+    let reverseArray = [];
+    for (let i = 0; i < s.length; i++)
+    {
+        reverseArray[i] = stringArray[s.length - i - 1];
+    }
+    return reverseArray.join('')
 };
 
 // Do not edit below this line
